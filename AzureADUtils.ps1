1dba875517c919540e2bbfcb7ed83bf614cb89a2
Matthew Ryrie <matthew.ryrie@ryrieconsulting.ca>
2020-02-13 20:48:55 -0700

    Updated Utils and added functionality for getting CSP subscriptions

--- AzureADUtils.ps1
+++ AzureADUtils.ps1
@@ -1,52 +1,61 @@
-﻿Connect-AzureAD
+﻿function Export-AzureADUsers {
+    [CmdletBinding(PositionalBinding=$false)]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
 
-$AADUsers = Get-AzureADUser -All $true | Select DisplayName, UserPrincipalName, Roles, MemberOf
-$AADDirectoryRoles = Get-AzureADDirectoryRole
-$AADGroups = Get-AzureADGroup
+    Connect-AzureAD
 
-$htAADDirectoryRoles = $null
-$htAADGroups = $null
+    $AADUsers = Get-AzureADUser -All $true | Select DisplayName, UserPrincipalName, Roles, MemberOf
+    $AADDirectoryRoles = Get-AzureADDirectoryRole
+    $AADGroups = Get-AzureADGroup
 
-foreach ($AADDirectoryRole in $AADDirectoryRoles){
-    $htAADDirectoryRoles += @{ $AADDirectoryRole.DisplayName = ((Get-AzureADDirectoryRoleMember -ObjectId $AADDirectoryRole.ObjectId).UserPrincipalName) }
-}
+    $htAADDirectoryRoles = $null
+    $htAADGroups = $null
 
-foreach ($AADGroup in $AADGroups){
-    $htAADGroups += @{ $AADGroup.DisplayName = ((Get-AzureADGroupMember -ObjectId $AADGroup.ObjectId).UserPrincipalName) }
-}
-
-#Get the roles that the users hold
-foreach ($AADUser in $AADUsers) {
-
-    $AADUser.Roles = $null
-
-    foreach ($roleName in $htAADDirectoryRoles.Keys){
-        $htAADDirectoryRole = $htAADDirectoryRoles[$roleName]
-        if ($htAADDirectoryRole -ne $null){
-            if ($htAADDirectoryRole.Contains($AADUser.UserPrincipalName)) {
-                $AADUser.Roles += "$roleName,"
-            }    
-        }
+    foreach ($AADDirectoryRole in $AADDirectoryRoles){
+        $htAADDirectoryRoles += @{ $AADDirectoryRole.DisplayName = ((Get-AzureADDirectoryRoleMember -ObjectId $AADDirectoryRole.ObjectId).UserPrincipalName) }
     }
 
-    if ($AADUser.Roles -ne $null) {
-        $AADUser.Roles = $AADUser.Roles.TrimEnd(",")
+    foreach ($AADGroup in $AADGroups){
+        $htAADGroups += @{ $AADGroup.DisplayName = ((Get-AzureADGroupMember -ObjectId $AADGroup.ObjectId).UserPrincipalName) }
     }
 
-    $AADUser.MemberOf = $null
+    #Get the roles that the users hold
+    foreach ($AADUser in $AADUsers) {
 
-    foreach ($AADGroupName in $htAADGroups.Keys){
-        $htAADGroup = $htAADGroups[$AADGroupName]
-        if ($htAADGroup -ne $null){
-            if ($htAADGroup.Contains($AADUser.UserPrincipalName)) {
-                $AADUser.MemberOf += "$AADGroupName,"
+        $AADUser.Roles = $null
+
+        foreach ($roleName in $htAADDirectoryRoles.Keys){
+            $htAADDirectoryRole = $htAADDirectoryRoles[$roleName]
+            if ($htAADDirectoryRole -ne $null){
+                if ($htAADDirectoryRole.Contains($AADUser.UserPrincipalName)) {
+                    $AADUser.Roles += "$roleName,"
+                }    
             }
         }
+
+        if ($AADUser.Roles -ne $null) {
+            $AADUser.Roles = $AADUser.Roles.TrimEnd(",")
+        }
+
+        $AADUser.MemberOf = $null
+
+        foreach ($AADGroupName in $htAADGroups.Keys){
+            $htAADGroup = $htAADGroups[$AADGroupName]
+            if ($htAADGroup -ne $null){
+                if ($htAADGroup.Contains($AADUser.UserPrincipalName)) {
+                    $AADUser.MemberOf += "$AADGroupName,"
+                }
+            }
+        }
+
+        if ($AADUser.MemberOf -ne $null) {
+            $AADUser.MemberOf = $AADUser.MemberOf.TrimEnd(",")
+        }
     }
 
-    if ($AADUser.MemberOf -ne $null) {
-        $AADUser.MemberOf = $AADUser.MemberOf.TrimEnd(",")
-    }
+    $AADUsers | Export-Csv -Path $Path -NoTypeInformation
+
 }
-
-$AADUsers | Export-Csv -Path D:\temp\AADUsers.csv -NoTypeInformation
---
+++ GetCSPAzureSubscriptions.ps1
@@ -0,0 +1,67 @@
+﻿Connect-AzAccount
+
+#List CSP subscriptions that we have access to
+$customers = Get-PartnerCustomer
+
+$customerRecords = @()
+
+$i = 0
+
+foreach ($customer in $customers){
+
+  $i+=1
+  Write-Progress -Activity 'Querying CSP customers' -Status $customer.Name -PercentComplete (($i/$customers.Count)*100)
+  <#
+  $azureSubs = Get-AzSubscription -TenantId $customer.CustomerId
+  $azureSubRecords = @()
+
+  foreach ($azureSub in $azureSubs){
+    $azureSubRecord = [pscustomobject]@{
+      Id = $azureSub.Id
+      Name 
+    }
+  }
+  #>
+
+  $customerRecord = [pscustomobject]@{
+    Name = $customer.Name
+    TenantId = $customer.CustomerId
+    AzureSubscriptions = (Get-AzSubscription -TenantId $customer.CustomerId)
+  }
+
+  $customerRecords += $customerRecord
+}
+
+[pscustomobject]@{
+  Name = $customer.Name
+  TenantId = $customer.CustomerId
+  AzureSubscriptions = (
+    [pscustomobject]@{
+      subscriptionId = $customer
+    }
+  )
+}
+
+$customersWithAzureSubs = $customerRecords | Where-Object {$_.AzureSubscriptions -ne $null}
+
+$subsCSP = $customersWithAzureSubs.AzureSubscriptions
+
+#Get non-CSP subscriptions that we have access to
+$subsNonCSP = Get-AzSubscription
+
+$subsAll = $subsCSP + $subsNonCSP
+
+$enabledCSPSubs = $subsCSP | Where-Object {$_.State -eq 'enabled'}
+
+$enabledSubs = $subsAll | Where-Object {$_.State -eq 'enabled'}
+
+$VMs = @()
+$i = 0
+
+foreach ($enabledSub in $enabledSubs){
+    $i+=1
+    Write-Progress -Activity 'Getting VMs' -Status $enabledSub.Name -PercentComplete (($i/$enabledSubs.Count)*100)
+    #Write-Host "SubscriptionID is $($enabledSub.Id), Tenant ID is $($enabledSub.TenantId)"
+    Select-AzSubscription -Subscription $enabledSub.Id -Tenant $enabledSub.TenantId
+    $VMs += Get-AzVM
+}
